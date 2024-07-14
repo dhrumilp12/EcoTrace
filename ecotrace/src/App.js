c56b74bba9adf78bb6fafd4df1ae9d9628285e79
vaishnavi-3969 <vaishnavi.kale3011@gmail.com>
2024-07-15 02:22:09 +0530

    basic app initialized

--- ecotrace/src/App.js
+++ ecotrace/src/App.js
@@ -1,11 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Landing from './pages/Landing';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
 
 function App() {
+  const routes = [
+    { path: '/', element: <Landing /> },
+    { path: '/home', element: <Home /> },
+    { path: '/profile', element: <Profile /> }
+  ]
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          {routes.map((route, index) => {
+            return <Route
+              key={index}
+              path={route.path}
+              element={route.element}
+              exact
+            />
+          })}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
